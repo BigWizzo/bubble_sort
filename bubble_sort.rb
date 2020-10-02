5b6e25160cfc242cd3a18ade53edce796b9e6457
Kingsley <kingobaino@gmail.com>
2020-10-02 22:08:23 +0100

    Fixed linters error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,12 @@
 def bubble_sort(array)
+  x = 0
   length = array.length
-	x = 0
-	while x < length - 1
-		if !array[x + 1].nil? && array[x] > array[x + 1]
-    array[x], array[x + 1] = array[x + 1], array[x]
-    x = 0
-		end
-		x += 1
+  while x < length - 1
+    if !array[x + 1].nil? && array[x] > array[x + 1]
+      array[x], array[x + 1] = array[x + 1], array[x]
+      x = 0
+    end
+    x += 1
 	end
 	p array
 end
@@ -17,7 +17,7 @@ def bubble_sort_by(array)
   length = array.length
   x = 0
   while x < length - 1
-    if !array[x + 1].nil?
+    unless array[x + 1] == nil
       yield array[x], array[x + 1]
       if array[x].length > array[x + 1].length
         array[x], array[x + 1] = array[x + 1], array[x]
