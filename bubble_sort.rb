80e7ffa9a5450b1b3efde6fd223212845024b6a6
bigwizzo <wnnwillard@gmail.com>
2020-10-02 22:45:42 +0200

    Fixing linters errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,34 +1,34 @@
 def bubble_sort(array)
-    length = array.length
-    x = 0
-    while x < length - 1 do
-            if (array[x+1] != nil) && (array[x] > array[x + 1])
-                    array[x], array[x + 1] = array[x + 1], array[x]
-                    x = 0                             
-            end           
-        x += 1
-    end
-    p array
+	length = array.length	
+	x = 0
+	while x < length - 1
+		if !array[x + 1].nil? && array[x] > array[x + 1]
+			array[x], array[x + 1] = array[x + 1], array[x]
+			x = 0
+		end
+		x += 1
+	end
+	p array
 end
 
-bubble_sort([2,-5,2,0,1,4,17])
+bubble_sort([2, -5, 2, 0, 1, 4, 17])
 
 def bubble_sort_by(array)
-    length = array.length
-    x = 0
-    while x < length - 1 do
-            if (array[x+1] != nil) 
-                yield array[x], array[x + 1]
-                if(array[x].length > array[x + 1].length)
-                       array[x], array[x + 1] = array[x + 1], array[x]
-                       x = 0  
-                end                           
-            end           
-        x += 1
+  length = array.length
+  x = 0
+  while x < length - 1
+    if !array[x + 1].nil?
+      yield array[x], array[x + 1]
+      if array[x].length > array[x + 1].length
+        array[x], array[x + 1] = array[x + 1], array[x]
+        x = 0
+      end
     end
-    p array
+    x += 1
+  end
+  p array
 end
 
-bubble_sort_by(%w[hi, hello, hey]) do |left, right|
-    left.length - right.length
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
 end
