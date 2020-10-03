80a03f46abcd3740e271b7780782b6b7e53a102f
Kingsley <kingobaino@gmail.com>
2020-10-03 08:33:00 +0100

    Effected the reviewers suggestions

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,14 +4,12 @@ def bubble_sort(array)
   while x < length - 1
     if !array[x + 1].nil? && array[x] > array[x + 1]
       array[x], array[x + 1] = array[x + 1], array[x]
-      x = 0
+      x = -1
     end
     x += 1
   end
-  p array
+   array
 end
-
-bubble_sort([2, -5, 2, 0, 1, 4, 17])
 
 def bubble_sort_by(array)
   length = array.length
@@ -26,9 +24,5 @@ def bubble_sort_by(array)
     end
     x += 1
   end
-  p array
-end
-
-bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
+   array
 end
